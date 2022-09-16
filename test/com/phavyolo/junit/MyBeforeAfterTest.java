0b18b34dd0a035a22da53a33c95d71240610f818
phavour <phavyolo@gmail.com>
2022-09-16 22:01:34 +0100

    exploring few important JUnit annotations

---
+++ test/com/phavyolo/junit/MyBeforeAfterTest.java
@@ -0,0 +1,49 @@
+package com.phavyolo.junit;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class MyBeforeAfterTest {
+	
+	//before --> setup
+	//after  --> cleanup
+	
+	@BeforeAll
+	static void beforeAll() {
+		System.out.println("BeforeAll");
+	}
+	
+	@BeforeEach
+	void beforeEach() {
+		System.out.println("BeforeEach");
+	}
+
+	@Test
+	void test1() {
+		System.out.println("test1");
+	}
+	
+	@Test
+	void test2() {
+		System.out.println("test2");
+	}
+	
+	@Test
+	void test3() {
+		System.out.println("test3");
+	}
+	
+	@AfterEach
+	void afterEach() {
+		System.out.println("AfterEach");
+	}
+	
+	@AfterAll
+	void afterAll() {
+		System.out.println("AfterAll");
+	}
+
+}
